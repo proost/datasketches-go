9b0ccc3833cdf6543bb46890771d63fa5643fe7b
lani_karrot <lani@daangn.com>
2026-02-18 13:12:01 +0900

    refactor: use built-in min function

--- count/utils.go
+++ count/utils.go
@@ -20,16 +20,7 @@ package count
 import (
 	"errors"
 	"math"
-
-	"golang.org/x/exp/constraints"
 )
-
-func Min[T constraints.Ordered](a, b T) T {
-	if a < b {
-		return a
-	}
-	return b
-}
 
 func SuggestNumBuckets(relativeError float64) (int32, error) {
 	if relativeError <= 0 {
@@ -42,7 +33,7 @@ func SuggestNumHashes(confidence float64) (int8, error) {
 	if confidence < 0 || confidence > 1.0 {
 		return 0, errors.New("confidence must be between 0 and 1.0 (inclusive)")
 	}
-	return Min(int8(math.Ceil(math.Log(1.0/(1.0-confidence)))), int8(math.MaxInt8)), nil
+	return min(int8(math.Ceil(math.Log(1.0/(1.0-confidence)))), int8(math.MaxInt8)), nil
 }
 
 func checkHeaderValidity(preamble, serVer, familyID, flagsByte byte) error {
